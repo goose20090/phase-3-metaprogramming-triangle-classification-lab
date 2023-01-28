2dd463397456c8e603928fa8352378d452900e84
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-01-28 18:07:42 +0000

    commit tree 61081ba2f03f9033a7428527d7d946973d3d7180

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize side_1, side_2, side_3
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  class TriangleError < StandardError
+    def message
+      'This is not a valid traingle'
+    end
+  end
+
+  def kind
+    if side_1 <= 0 || side_2 <= 0 || side_3 <= 0
+      raise TriangleError
+    elsif side_1 + side_2 <= side_3 || side_1 + side_3 <= side_2 || side_2 + side_3 <= side_1
+      raise TriangleError
+    end
+    return :equilateral if side_1 == side_2 && side_2 ==side_3 && side_1 == side_3
+    return :isosceles if side_1 == side_2 || side_2 == side_3 || side_1 == side_3
+
+    return :scalene
+  end
 end
